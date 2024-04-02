a02d9c6f3fd1760743a75753ec0a2ae420990a40
zoemccuen <113805460+zoemccuen@users.noreply.github.com>
2024-04-02 15:08:18 -0400

    mongo

---
+++ index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://zoemccuen:zoespassword@assignment16.unb1sxv.mongodb.net/?retryWrites=true&w=majority&appName=assignment16")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const schema = new mongoose.Schema({
+  name: String,
+});
+
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
+
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World - From Zoe!",
+});
+
+createMessage();
